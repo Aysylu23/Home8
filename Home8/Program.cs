95c5f9b55cf41b84811d5504e26a105885483860
Aysylu23 <aysylu_bad@mail.ru>
2024-04-29 13:43:56 +0500

    Добавьте файлы проекта.

---
+++ Home8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Объедините две предыдущих работы (практические работы 2 и 3): поиск файла и поиск текста в файле написав утилиту которая ищет файлы определенного расширения с указанным текстом.
+// Рекурсивно. Пример вызова утилиты: utility.exe txt текст.
+namespace Home8
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            HomeWork.FindFiles("C:\\Users\\USER\\source\\repos", "*.txt", list);
+
+            foreach (string file in list)
+            {
+                if (HomeWork.FindWord(file, "text") == true)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+        }
+    }
+}
